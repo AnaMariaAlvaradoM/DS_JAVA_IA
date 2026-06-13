6ba1156c38aacacdf6c13e15fd1b34c14b8af1d9
 <anaa_alvarad@hotmail.com>
2026-06-13 15:19:19 -0500

    Subo arcihvo de repaso clase 1

---
+++ Clase2/otraClase.java
@@ -0,0 +1,13 @@
+package Clase2;
+
+public class otraClase {
+    public static void main(String[] args) {
+        int numero = 20;
+        String texto = "Adiós Mundo";
+        boolean esFalso = false;
+
+        System.out.println("Número: " + numero);
+        System.out.println("Texto: " + texto);
+        System.out.println("Es falso: " + esFalso);
+    }
+}
