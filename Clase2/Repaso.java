9c018681b72377053c74a4743876ac904bd3984b
 <anaa_alvarad@hotmail.com>
2026-06-13 15:16:50 -0500

    Subo arcihvo de repaso clase 1

---
+++ Clase2/Repaso.java
@@ -0,0 +1,13 @@
+package Clase2;
+
+public class Repaso {
+    public static void main(String[] args) {
+        int numero = 10;
+        String texto = "Hola Mundo";
+        boolean esVerdadero = true;
+
+        System.out.println("Número: " + numero);
+        System.out.println("Texto: " + texto);
+        System.out.println("Es verdadero: " + esVerdadero);
+    }
+}
