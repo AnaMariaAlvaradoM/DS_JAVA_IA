8539e882947b67eefe8d7f58abb48c4c70a57150
 <anaa_alvarad@hotmail.com>
2026-06-20 16:02:22 -0500

    Todos los archivos de la clase

---
+++ clase3/clase3/src/Metodos.java
@@ -0,0 +1,12 @@
+public class Metodos {
+    public static void main(String[] args) {
+        double propina = calcularPropina(80000, 0.10);
+        System.out.println("Propina: " + propina);
+    }
+
+    public static double calcularPropina(double subtotal, double porcentaje) {
+        return subtotal * porcentaje;
+    }
+
+    
+}
